ab9c836b1222771140b91f75a8f2a9d07f56aa72
Pedro Leal <pedroleal360@gmail.com>
2024-03-17 21:20:30 -0300

    numeroprimo

--- Aulas/secao1_Fundamentos/_01calcularnumeros/src/NumeroPrimo.rs
+++ Aulas/secao1_Fundamentos/_01calcularnumeros/src/NumeroPrimo.rs
@@ -1,8 +1,11 @@
-pub fn verificarPrimo(n: [i32;9]) -> i32 {
-    let constSqrt = n.sqrt();
-    let array = [1.=constSqrt];
+pub fn verificarPrimo(n: i32) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    let limite: f64 = (n as f64).sqrt().ceil();
+    let limite32 = limite as i32;
     let mut contador = 0;
-    for i in array {
+    for i in 1..=limite32 {
         if i % 2 == 0 {
             contador += 1;
         }
--- Aulas/secao1_Fundamentos/_01calcularnumeros/src/main.rs
+++ Aulas/secao1_Fundamentos/_01calcularnumeros/src/main.rs
@@ -3,6 +3,7 @@ mod NumeroPrimo;
 
 fn main() {
     let vetor1= [1,123,15,354,624,1632, 335,3,5];
-    println!("o maior nª é {}", MaiorNumeroVetor::bigger_n(vetor1));
-    println!("o número primo é {}", NumeroPrimo::verificarPrimo(vetor1));
+    let testPrimo = 5;
+    //println!("o maior nª é {}", MaiorNumeroVetor::bigger_n(vetor1));
+    println!("o número primo é {}", NumeroPrimo::verificarPrimo(testPrimo));
 }
