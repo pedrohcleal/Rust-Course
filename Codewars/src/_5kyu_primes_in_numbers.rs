aceb9aa700763b04102c6e5ac553a8cb35af1941
Pedro Leal <pedroleal360@gmail.com>
2024-04-05 19:36:50 -0300

    up

--- Codewars/src/_5kyu_primes_in_numbers.rs
+++ Codewars/src/_5kyu_primes_in_numbers.rs
@@ -1,32 +1,26 @@
+use std::cmp::max;
 use std::collections::{BTreeMap};
 
 pub fn prime_factors(mut n: i64) -> String {
-    let nprimos: [i64; 25] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-    let mut finalstr = String::new();
-    let mut index: usize = 24;
-    let mut cont: BTreeMap<i64,i64> = BTreeMap::new();
-
-    while n > 1 {
-        if n % nprimos[index] == 0{
-            println!("{:?}", cont);
-            n /= nprimos[index];
-            let contador = cont.entry((nprimos[index])).or_insert(0);
-            *contador +=1;
+    let mut number = n;
+    let mut prime_numbers = vec![];
+    let mut factor = 2;
+    let mut cnt;
+    while number > 1 {
+        cnt = 0;
+        while number % factor == 0 {
+            number /= factor;
+            cnt += 1;
         }
-        else {
-            index -= 1;
+        if cnt > 0 {
+            if cnt > 1
+            {prime_numbers.push(format!("({}**{})", factor, cnt));}
+            else
+            {prime_numbers.push(format!("({})", factor));}
         }
+        factor += 1;
     }
-
-    for (key, value) in &cont {
-        if *value == 1{
-            finalstr.push_str(&format!("({key})"));
-        }
-        else {
-            finalstr.push_str(&format!("({key}**{value})"))
-        }
-    }
-    return finalstr.to_string();
+    prime_numbers.join("")
 }
 /*Given a positive number n > 1 find the prime factor decomposition of n.
 The result will be a string with the following form :
