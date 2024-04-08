4a2c272f5cfab761859bd00b8ab0c84a2b3dc17e
Pedro Leal <pedroleal360@gmail.com>
2024-04-08 16:22:44 -0300

    update

---
+++ Codewars/src/_6kyu_counting_duplicates.rs
@@ -0,0 +1,17 @@
+use std::fmt::format;
+
+pub fn count_duplicates(text: &str) -> u32 {
+    let mut text = text.to_string().to;
+    let mut cont1:u32 = 0;
+    for i in text.to_lowercase().chars().rev(){
+        println!("{}", i);
+        let soma = text.to_lowercase().chars().filter(|&c| c == i).count();
+        if soma > 1 {
+            cont1 += 1;
+            text = text.replacen(char::from(i), "", soma);
+            //println!("{}", text.replacen(char::from(i), "", soma))
+        }
+    }
+    return  cont1
+    // Your code goes here
+}
--- Codewars/src/main.rs
+++ Codewars/src/main.rs
@@ -1,4 +1,5 @@
 use crate::_5kyu_primes_in_numbers::prime_factors;
+use crate::_6kyu_counting_duplicates::count_duplicates;
 use crate::_6kyu_two_sum::two_sum;
 use crate::_6kyu_unique_in_order::unique_in_order;
 use crate::_7kyu_count_the_digit::nb_dig;
@@ -12,9 +13,14 @@ mod _7kyu_square_every_digit;
 mod _6kyu_two_sum;
 mod _7kyu_count_the_digit;
 mod _5kyu_primes_in_numbers;
+mod _6kyu_counting_duplicates;
 
 fn main() {
-    println!("Start");
-    nb_dig(550,5);
-    println!("{}", prime_factors(17*17*93*677));
+    println!("Starrrt");
+    println!("{}", count_duplicates("woorld"));
 }
+
+// Escreva uma função que retornará a contagem de caracteres alfabéticos distintos,
+// sem distinção entre maiúsculas e minúsculas, e dígitos numéricos que ocorrem mais de uma vez
+// na string de entrada. Pode-se presumir que a string de entrada contém apenas alfabetos (maiúsculas e minúsculas)
+// e dígitos numéricos.
